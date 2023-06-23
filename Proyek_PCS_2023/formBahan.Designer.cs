d48fc88a5e14327feb4f267aeea78c835451467f
Kimbooo28 <66553277+ClusterID@users.noreply.github.com>
2023-06-23 13:31:54 +0700

    Bahan

--- Proyek_PCS_2023/formBahan.Designer.cs
+++ Proyek_PCS_2023/formBahan.Designer.cs
@@ -31,165 +31,188 @@ namespace Proyek_PCS_2023
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxNamaBahan = new System.Windows.Forms.TextBox();
+            this.textBoxSatuan = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownStock = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStock)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 38);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 31);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(478, 400);
+            this.dataGridView1.Size = new System.Drawing.Size(358, 325);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentDoubleClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Bahan";
             // 
-            // button1
+            // buttonInsert
             // 
-            this.button1.Location = new System.Drawing.Point(501, 169);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Insert";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonInsert.Location = new System.Drawing.Point(376, 137);
+            this.buttonInsert.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(56, 19);
+            this.buttonInsert.TabIndex = 2;
+            this.buttonInsert.Text = "Insert";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // buttonUpdate
             // 
-            this.button2.Location = new System.Drawing.Point(582, 169);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Update";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Location = new System.Drawing.Point(436, 137);
+            this.buttonUpdate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(56, 19);
+            this.buttonUpdate.TabIndex = 3;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
             // 
-            // button3
+            // buttonDelete
             // 
-            this.button3.Location = new System.Drawing.Point(663, 169);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Delete";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonDelete.Location = new System.Drawing.Point(497, 137);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(56, 19);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(522, 46);
+            this.label2.Location = new System.Drawing.Point(392, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "ID Bahan";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(522, 74);
+            this.label3.Location = new System.Drawing.Point(392, 60);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(90, 17);
+            this.label3.Size = new System.Drawing.Size(69, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Nama Bahan";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(522, 104);
+            this.label4.Location = new System.Drawing.Point(392, 84);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(81, 17);
+            this.label4.Size = new System.Drawing.Size(63, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = "Stok Bahan";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(522, 132);
+            this.label5.Location = new System.Drawing.Point(392, 107);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(98, 17);
+            this.label5.Size = new System.Drawing.Size(75, 13);
             this.label5.TabIndex = 8;
             this.label5.Text = "Satuan Bahan";
             // 
-            // textBox1
+            // textBoxID
             // 
-            this.textBox1.Location = new System.Drawing.Point(636, 43);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 9;
+            this.textBoxID.Location = new System.Drawing.Point(477, 35);
+            this.textBoxID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(76, 20);
+            this.textBoxID.TabIndex = 9;
             // 
-            // textBox2
+            // textBoxNamaBahan
             // 
-            this.textBox2.Location = new System.Drawing.Point(636, 71);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 10;
+            this.textBoxNamaBahan.Location = new System.Drawing.Point(477, 58);
+            this.textBoxNamaBahan.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxNamaBahan.Name = "textBoxNamaBahan";
+            this.textBoxNamaBahan.Size = new System.Drawing.Size(76, 20);
+            this.textBoxNamaBahan.TabIndex = 10;
             // 
-            // textBox4
+            // textBoxSatuan
             // 
-            this.textBox4.Location = new System.Drawing.Point(636, 129);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
-            this.textBox4.TabIndex = 12;
+            this.textBoxSatuan.Location = new System.Drawing.Point(477, 105);
+            this.textBoxSatuan.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxSatuan.Name = "textBoxSatuan";
+            this.textBoxSatuan.Size = new System.Drawing.Size(76, 20);
+            this.textBoxSatuan.TabIndex = 12;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(661, 415);
+            this.button4.Location = new System.Drawing.Point(496, 337);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(56, 19);
             this.button4.TabIndex = 13;
             this.button4.Text = "Back";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // numericUpDown1
+            // numericUpDownStock
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(636, 101);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(100, 22);
-            this.numericUpDown1.TabIndex = 14;
+            this.numericUpDownStock.Location = new System.Drawing.Point(477, 82);
+            this.numericUpDownStock.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDownStock.Name = "numericUpDownStock";
+            this.numericUpDownStock.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDownStock.TabIndex = 14;
             // 
             // formBahan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(748, 450);
-            this.Controls.Add(this.numericUpDown1);
+            this.ClientSize = new System.Drawing.Size(561, 366);
+            this.Controls.Add(this.numericUpDownStock);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxSatuan);
+            this.Controls.Add(this.textBoxNamaBahan);
+            this.Controls.Add(this.textBoxID);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonInsert);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "formBahan";
             this.Text = "formBahan";
+            this.Load += new System.EventHandler(this.formBahan_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStock)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -199,17 +222,17 @@ namespace Proyek_PCS_2023
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxNamaBahan;
+        private System.Windows.Forms.TextBox textBoxSatuan;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDownStock;
     }
 }
--- Proyek_PCS_2023/formBahan.cs
+++ Proyek_PCS_2023/formBahan.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Proyek_PCS_2023
 {
@@ -16,10 +17,114 @@ namespace Proyek_PCS_2023
         {
             InitializeComponent();
         }
+        DataTable dataBahan = new DataTable();
 
         private void button4_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void formBahan_Load(object sender, EventArgs e)
+        {
+            dataBahan.Columns.Add("ID_BAHAN");
+            dataBahan.Columns.Add("NAMA");
+            dataBahan.Columns.Add("STOK");
+            dataBahan.Columns.Add("SATUAN");
+
+            dataBahan = DB.query("SELECT * FROM BAHAN");
+
+            updatedatacart();
+        }
+
+        void updatedatacart()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = dataBahan;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strtotalbahan = DB.getScalar("SELECT COUNT(*) FROM BAHAN");
+
+            int inttotalbahan = Int32.Parse(strtotalbahan);
+            inttotalbahan++;
+            string idbahan = "B00" + inttotalbahan.ToString();
+
+
+            string tempnamabahan = textBoxNamaBahan.Text;
+            int tempstok = Convert.ToInt32(numericUpDownStock.Value);
+            string tempsatuanbahan = textBoxSatuan.Text;
+
+
+            MySqlCommand cmdins = new MySqlCommand($"INSERT INTO BAHAN VALUES(@idbahan,@nama,@stok,@satuan)", DB.conn);
+            cmdins.Parameters.AddWithValue("@idbahan", idbahan);
+            cmdins.Parameters.AddWithValue("@nama", tempnamabahan);
+            cmdins.Parameters.AddWithValue("@stok", tempstok);
+            cmdins.Parameters.AddWithValue("@satuan", tempsatuanbahan);
+
+            DB.open();
+
+            try
+            {
+                cmdins.ExecuteNonQuery();
+                updatedatacart();
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.Message);
+            }
+
+            DB.close();
+
+            MessageBox.Show("Berhasil Insert Bahan");
+        }
+
+        private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+
+            if (index < 0)
+            {
+                return;
+            }
+
+            DataGridViewRow row = dataGridView1.Rows[index];
+            string tempidbahan = row.Cells["ID_BAHAN"].Value.ToString();
+            string tempnamabahan = row.Cells["NAMA"].Value.ToString();
+            string tempstok = row.Cells["STOK"].Value.ToString();
+            string tempsatuan = row.Cells["SATUAN"].Value.ToString();
+
+            int intsatuan = Convert.ToInt32.Parse(tempsatuan);
+
+            textBoxNamaBahan.Text = tempnamabahan;
+            numericUpDownStock.Value = intsatuan;
+            textBoxSatuan.Text = tempsatuan;
+
+            
+
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            string tempnama = textBoxNamaBahan.Text;
+            int tempstok = numericUpDownStock.Value;
+            string satuan = textBoxSatuan.Text;
+            string idbahan = textBoxID.Text;
+            
+            MySqlCommand commandUpdate = new MySqlCommand("UPDATE BAHAN SET NAMA = @nama, STOK = @stok, SATUAN = @satuan WHERE ID = @id", DB.conn);
+            commandUpdate.Parameters.AddWithValue("@nama", tempnama);
+            commandUpdate.Parameters.AddWithValue("@stok", tempstok);
+            commandUpdate.Parameters.AddWithValue("@satuan", satuan);
+            commandUpdate.Parameters.AddWithValue("@id", idbahan);
+
+            updatedatacart();
+
+            MessageBox.Show("Berhasil Update Barang");
         }
     }
 }
