265a5b57ff292fe9782a5ffc5f605d4ffcefc7bc
rocky1009 <105706296+rocky1009@users.noreply.github.com>
2023-06-23 15:26:50 +0700

    kitchen bisa kurangi stock di bahan secara otomatis saat cooking mulai

--- Proyek_PCS_2023/formKitchen.cs
+++ Proyek_PCS_2023/formKitchen.cs
@@ -35,7 +35,43 @@ namespace Proyek_PCS_2023
 
         private void button1_Click(object sender, EventArgs e)
         {
-            UpdateStatus("COOKING");
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                int rowIndex = dataGridView1.SelectedCells[0].RowIndex;
+                DataGridViewRow row = dataGridView1.Rows[rowIndex];
+
+                string makanan = row.Cells["Makanan"].Value.ToString();
+                int id_fnb = GetFnbId(makanan);
+
+                if (id_fnb != -1)
+                {
+                    List<int> id_bahanList = GetBahanIds(id_fnb);
+
+                    if (id_bahanList.Count > 0)
+                    {
+                        foreach (int id_bahan in id_bahanList)
+                        {
+                            ReduceBahanStock(id_bahan);
+                        }
+
+                        // Update the status to "COOKING"
+                        row.Cells["Status"].Value = "COOKING";
+                        UpdateRowBackgroundColor(row);
+
+                        // Update the status in the h_trans table
+                        string nomorNotaHTrans = row.Cells["Nomor Nota"].Value.ToString();
+                        UpdateHTransStatus(nomorNotaHTrans, "COOKING");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No ingredients found for the selected food item.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Food item not found in the database.");
+                }
+            }
         }
 
         private void dataGridView1_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
@@ -64,12 +100,38 @@ namespace Proyek_PCS_2023
             }
         }
 
-        int id, idx;
-        string status;
-
         private void button2_Click(object sender, EventArgs e)
         {
-            UpdateStatus("DONE");
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                int rowIndex = dataGridView1.SelectedCells[0].RowIndex;
+                DataGridViewRow row = dataGridView1.Rows[rowIndex];
+
+                string nomorNotaHTrans = row.Cells["Nomor Nota"].Value.ToString();
+
+                // Update the status to "DONE"
+                row.Cells["Status"].Value = "DONE";
+                UpdateRowBackgroundColor(row);
+
+                // Update the status in the h_trans table
+                UpdateHTransStatus(nomorNotaHTrans, "DONE");
+            }
+        }
+
+        private void UpdateHTransStatus(string nomorNotaHTrans, string status)
+        {
+            string updateQuery = "UPDATE h_trans SET STATUS = @status WHERE NOMOR_NOTA_HTRANS = @nomorNotaHTrans";
+
+            using (MySqlConnection connection = new MySqlConnection("server=localhost;user id=root;database=db_proyek_pcs_2023"))
+            {
+                connection.Open();
+                using (MySqlCommand command = new MySqlCommand(updateQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@status", status);
+                    command.Parameters.AddWithValue("@nomorNotaHTrans", nomorNotaHTrans);
+                    command.ExecuteNonQuery();
+                }
+            }
         }
 
         private void UpdateStatus(string newStatus)
@@ -99,6 +161,69 @@ namespace Proyek_PCS_2023
             else if (status == "DONE")
             {
                 row.DefaultCellStyle.BackColor = Color.Green;
+            }
+        }
+
+        private int GetFnbId(string nama_fnb)
+        {
+            int id_fnb = -1;
+            string query = "SELECT id_fnb FROM fnb WHERE nama_fnb = @nama_fnb";
+
+            using (MySqlConnection connection = new MySqlConnection("server=localhost;user id=root;database=db_proyek_pcs_2023"))
+            {
+                connection.Open();
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@nama_fnb", nama_fnb);
+                    object result = command.ExecuteScalar();
+
+                    if (result != null && result != DBNull.Value)
+                    {
+                        id_fnb = Convert.ToInt32(result);
+                    }
+                }
+            }
+
+            return id_fnb;
+        }
+
+        private List<int> GetBahanIds(int id_fnb)
+        {
+            List<int> id_bahanList = new List<int>();
+            string query = "SELECT id_bahan FROM resep WHERE id_fnb = @id_fnb";
+
+            using (MySqlConnection connection = new MySqlConnection("server=localhost;user id=root;database=db_proyek_pcs_2023"))
+            {
+                connection.Open();
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id_fnb", id_fnb);
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id_bahan = reader.GetInt32("id_bahan");
+                            id_bahanList.Add(id_bahan);
+                        }
+                    }
+                }
+            }
+
+            return id_bahanList;
+        }
+
+        private void ReduceBahanStock(int id_bahan)
+        {
+            string updateQuery = "UPDATE bahan SET stok = stok - 1 WHERE id_bahan = @id_bahan";
+
+            using (MySqlConnection connection = new MySqlConnection("server=localhost;user id=root;database=db_proyek_pcs_2023"))
+            {
+                connection.Open();
+                using (MySqlCommand command = new MySqlCommand(updateQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@id_bahan", id_bahan);
+                    command.ExecuteNonQuery();
+                }
             }
         }
     }
