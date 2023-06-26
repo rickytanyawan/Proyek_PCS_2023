004f8bd5e38eba99df90ad3c29469dfa7651cf59
Kimbooo28 <66553277+ClusterID@users.noreply.github.com>
2023-06-26 21:46:12 +0700

    History H_Trans

--- Proyek_PCS_2023/Master.Designer.cs
+++ Proyek_PCS_2023/Master.Designer.cs
@@ -40,9 +40,10 @@ namespace Proyek_PCS_2023
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 37);
+            this.button1.Location = new System.Drawing.Point(9, 30);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 52);
+            this.button1.Size = new System.Drawing.Size(86, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "Go to Bahan";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,9 +51,10 @@ namespace Proyek_PCS_2023
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 95);
+            this.button2.Location = new System.Drawing.Point(9, 77);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(114, 52);
+            this.button2.Size = new System.Drawing.Size(86, 42);
             this.button2.TabIndex = 1;
             this.button2.Text = "Go to Resep";
             this.button2.UseVisualStyleBackColor = true;
@@ -60,9 +62,10 @@ namespace Proyek_PCS_2023
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(105, 153);
+            this.button3.Location = new System.Drawing.Point(79, 124);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(114, 52);
+            this.button3.Size = new System.Drawing.Size(86, 42);
             this.button3.TabIndex = 2;
             this.button3.Text = "Menu Makanan";
             this.button3.UseVisualStyleBackColor = true;
@@ -70,18 +73,21 @@ namespace Proyek_PCS_2023
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(105, 211);
+            this.button6.Location = new System.Drawing.Point(79, 171);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(114, 52);
+            this.button6.Size = new System.Drawing.Size(86, 42);
             this.button6.TabIndex = 5;
             this.button6.Text = "Histori Transaksi";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(186, 37);
+            this.button4.Location = new System.Drawing.Point(140, 30);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(114, 52);
+            this.button4.Size = new System.Drawing.Size(86, 42);
             this.button4.TabIndex = 6;
             this.button4.Text = "Menjadi Customer";
             this.button4.UseVisualStyleBackColor = true;
@@ -89,9 +95,10 @@ namespace Proyek_PCS_2023
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(186, 95);
+            this.button5.Location = new System.Drawing.Point(140, 77);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(114, 52);
+            this.button5.Size = new System.Drawing.Size(86, 42);
             this.button5.TabIndex = 7;
             this.button5.Text = "Menjadi Chef";
             this.button5.UseVisualStyleBackColor = true;
@@ -101,18 +108,19 @@ namespace Proyek_PCS_2023
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(119, 9);
+            this.label1.Location = new System.Drawing.Point(89, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 25);
+            this.label1.Size = new System.Drawing.Size(64, 20);
             this.label1.TabIndex = 8;
             this.label1.Text = "Master";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Master
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(312, 274);
+            this.ClientSize = new System.Drawing.Size(234, 223);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -120,6 +128,7 @@ namespace Proyek_PCS_2023
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Master";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
